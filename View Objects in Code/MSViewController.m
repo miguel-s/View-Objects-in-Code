de0df5cdddfd78b251e6c65e45ea31971d5c3526
Miguel Serrano <Mimi@Mimi.local>
2014-06-01 20:09:21 +0200

    solution

--- View Objects in Code/MSViewController.m
+++ View Objects in Code/MSViewController.m
@@ -10,6 +10,10 @@
 
 @interface MSViewController ()
 
+@property UILabel *codeLabel;
+@property UITextField *codeTextField;
+@property UIButton *codeButton;
+
 @end
 
 @implementation MSViewController
@@ -18,12 +22,37 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect labelFrame = CGRectMake(20, 60, 280, 22);
+    CGRect textFieldFrame = CGRectMake(20, 110, 280, 30);
+    CGRect buttonFrame = CGRectMake(20, 160, 280, 30);
+    
+    self.codeLabel = [[UILabel alloc] initWithFrame:labelFrame];
+    self.codeLabel.text = @"Label Code";
+    self.codeLabel.backgroundColor = [UIColor redColor];
+    
+    self.codeTextField = [[UITextField alloc] initWithFrame:textFieldFrame];
+    self.codeTextField.placeholder = @"Tesxtfield Code";
+    self.codeTextField.backgroundColor = [UIColor blueColor];
+    
+    self.codeButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.codeButton.frame = buttonFrame;
+    [self.codeButton setTitle:@"Press me!" forState:UIControlStateNormal];
+    [self.codeButton addTarget:self action:@selector(didPressButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:self.codeLabel];
+    [self.view addSubview:self.codeTextField];
+    [self.view addSubview:self.codeButton];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)didPressButton:(UIButton *)button {
+    self.codeLabel.text = self.codeTextField.text;
 }
 
 @end
